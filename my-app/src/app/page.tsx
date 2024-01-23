8f6b2ff509fae1c2fe6f0027d84cf73d81b67624
Dang Thi <dangthi@MacBook-Pro-cua-Dang.local>
2024-01-23 15:28:41 +0700

    update Section Header

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,8 +1,7 @@
 import Banner from "@/components/layouts/Banner";
 import Header from "@/components/layouts/Header";
 import HomeMenu from "@/components/layouts/HomeMenu";
-import Image from "next/image";
-import Link from "next/link";
+import SectionHeader from "@/components/layouts/SectionHeader";
 
 export default function Home() {
   return (
@@ -10,6 +9,36 @@ export default function Home() {
       <Header />
       <Banner />
       <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeader 
+          mainHeader={"Our Story"}
+          subHeader={"About Us"}
+        />
+        <div className="max-w-2xl text-gray-500 mx-auto font-semibold flex flex-col gap-4 mt-4">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Eligendi autem, sit quidem excepturi aut molestiae rerum minima 
+            facere quae quos temporibus officia distinctio accusantium veniam ipsum fugiat corporis obcaecati assumenda.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Eligendi autem, sit quidem excepturi aut molestiae rerum minima 
+            facere quae quos temporibus officia distinctio accusantium veniam ipsum fugiat corporis obcaecati assumenda.
+          </p>
+        </div>
+      </section>
+      <section className="text-center my-8">
+        <SectionHeader 
+          mainHeader={"Don\'t hesitate"}
+          subHeader={"Contact us"}
+        />
+        <div className="mt-8">
+          <a className="underline text-4xl text-gray-500" href="tel:+8490090999">+84 90 090 999 </a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+        &copy; 2024 All rights reserved
+      </footer>
     </>
   );
 }
