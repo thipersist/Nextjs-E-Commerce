0468399f831883e9a3785a5ff9f9de303f6706e3
Dang Thi <dangthi@MacBook-Pro-cua-Dang.local>
2024-01-24 10:21:25 +0700

    update

--- my-app/src/app/register/page.tsx
+++ my-app/src/app/register/page.tsx
@@ -1,5 +1,20 @@
 export default function RegisterPage(){
     return (
-        <section>test</section>
+        <section className="mt-8">
+            <h1 className="text-center text-primary text-4xl">
+                Register
+            </h1>
+            <div className="absolute mx-auto items-center">
+                <div className="relative w-[350px] h-[340px] rounded-3xl shadow-2xl">
+                    <form>
+                        <input type="username" placeholder="username"/>
+                        <input type="email" placeholder="email"/>
+                        <input type="password" placeholder="password"/>
+                        <input type="password" placeholder="confirm password"/>
+                        <button className="submit">Register</button>
+                    </form>
+                </div>     
+            </div>
+        </section>
     )
 }
